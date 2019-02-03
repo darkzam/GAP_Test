78f71a2617d72e6a6b5e36f731c3980d0a6f6459
zamiralexisnarvaez@gmail.com <zamiralexisnarvaez@gmail.com>
2019-02-03 14:37:29 -0500

    v0.0.4 = Created RiskTypeController, Service, Entity.

---
+++ Services/Interface/IRiskTypeService.cs
@@ -0,0 +1,26 @@
+﻿using GAP.Insurance.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GAP.Insurance.Services.Interface
+{
+    public interface IRiskTypeService
+    {
+        List<RiskType> GetRiskTypes();
+
+        RiskType GetRiskTypeById(int id);
+
+        RiskType AddRiskType(RiskType RiskType);
+
+        void UpdateRiskType(RiskType RiskType);
+
+        void DeleteRiskType(RiskType RiskType);
+
+        bool RiskTypeExists(int id);
+
+        bool Save();
+
+    }
+}
